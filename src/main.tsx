da7113b17a22ab9525598b10561489eaa7af3746
Elnur A. Astanov <elnur.astanov@kapitalbank.az>
2023-01-23 14:42:01 +0400

    Created initial routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,47 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import { lazy, Suspense } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import { App } from "./App";
+import { Layout } from "./components";
+import { Paths } from "./ts/enums";
 
-const root = ReactDOM.createRoot(document.getElementById("root")!);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./index.css";
+
+const Login = lazy(() => import("./views/login"));
+const Templates = lazy(() => import("./views/templates"));
+const Resume = lazy(() => import("./views/resume"));
+
+const router = createBrowserRouter([
+  {
+    path: Paths.MAIN,
+    element: <Layout />,
+    children: [
+      {
+        path: Paths.LOGIN,
+        element: (
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <Login />
+          </Suspense>
+        )
+      },
+      {
+        path: Paths.TEMPLATES,
+        element: (
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <Templates />
+          </Suspense>
+        )
+      },
+      {
+        path: Paths.RESUME,
+        element: (
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <Resume />
+          </Suspense>
+        )
+      }
+    ]
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<RouterProvider router={router} />);
