0602046567d178567773720e79bbd94000d888af
Elnur A. Astanov <elnur.astanov@kapitalbank.az>
2023-01-23 14:57:29 +0400

    Splitted router codes

--- src/main.tsx
+++ src/main.tsx
@@ -1,47 +1,8 @@
 import ReactDOM from "react-dom/client";
-import { lazy, Suspense } from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 
-import { Layout } from "./components";
-import { Paths } from "./ts/enums";
+import { router } from "./routes";
 
 import "./index.css";
-
-const Login = lazy(() => import("./views/login"));
-const Templates = lazy(() => import("./views/templates"));
-const Resume = lazy(() => import("./views/resume"));
-
-const router = createBrowserRouter([
-  {
-    path: Paths.MAIN,
-    element: <Layout />,
-    children: [
-      {
-        path: Paths.LOGIN,
-        element: (
-          <Suspense fallback={<h1>Loading...</h1>}>
-            <Login />
-          </Suspense>
-        )
-      },
-      {
-        path: Paths.TEMPLATES,
-        element: (
-          <Suspense fallback={<h1>Loading...</h1>}>
-            <Templates />
-          </Suspense>
-        )
-      },
-      {
-        path: Paths.RESUME,
-        element: (
-          <Suspense fallback={<h1>Loading...</h1>}>
-            <Resume />
-          </Suspense>
-        )
-      }
-    ]
-  }
-]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<RouterProvider router={router} />);
