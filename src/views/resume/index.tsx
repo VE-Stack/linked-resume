103f032886f479490f9e35b485d9ba476d00cd84
Elnur A. Astanov <elnur.astanov@kapitalbank.az>
2023-01-23 14:59:00 +0400

    Renamed component

--- src/views/resume/index.tsx
+++ src/views/resume/index.tsx
@@ -2,13 +2,15 @@ import { Link } from "react-router-dom";
 
 import { Paths } from "../../ts/enums";
 
-const Login = () => {
+const Resume = () => {
   return (
     <div>
       <h1 className="text-2xl">Resume</h1>
-      <Link className="text-lg underline decoration-sky-500" to={Paths.LOGIN}>Route to login</Link>
+      <Link className="text-lg underline decoration-sky-500" to={Paths.LOGIN}>
+        Route to login
+      </Link>
     </div>
   );
 };
 
-export default Login;
+export default Resume;
