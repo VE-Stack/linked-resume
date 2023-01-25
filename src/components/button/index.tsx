a65a364a103d8dcd90b82afa785da09ebcd30705
MDReal32 <veysaliyev00@gmail.com>
2023-01-25 11:01:35 +0400

    button implementation

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,3 +1,26 @@
-export const Button = () => {
-    return <></>
-}
+import { ButtonHTMLAttributes, FC, PropsWithChildren } from "react";
+
+import { useGetClassesByFn } from "../../hooks/useGetClassesByFn";
+import { VariantNDesign } from "../../ts/types";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, VariantNDesign {}
+
+export const Button: FC<PropsWithChildren<ButtonProps>> = ({
+  variant,
+  design,
+  children,
+  ...btnProps
+}) => {
+  const getClasses = useGetClassesByFn("px-6", "py-2.5", "rounded-full");
+
+  return (
+    <button
+      data-design={design}
+      data-variant={variant}
+      className={getClasses(variant, design)}
+      {...btnProps}
+    >
+      {children}
+    </button>
+  );
+};
