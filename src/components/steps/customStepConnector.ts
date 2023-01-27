aeb574898e09c5ea5b6e9849164ca5cabd82e7f8
elnurastanov <elnurastanov@gmail.com>
2023-01-27 20:46:54 +0400

    Updated step components style

--- src/components/steps/customStepConnector.ts
+++ src/components/steps/customStepConnector.ts
@@ -7,18 +7,24 @@ export const CustomStepConnector = styled(StepConnector)(({ theme }) => ({
   },
   [`&.${stepConnectorClasses.active}`]: {
     [`& .${stepConnectorClasses.line}`]: {
-      backgroundImage: `linear-gradient( 95deg,${theme.palette.success.main} 0%,${theme.palette.primary.main})`
+      backgroundImage: `linear-gradient(95deg,rgba(46, 125, 50, 0.55) 0%,rgba(25, 118, 210, 0.55) 100%)`,
+      boxShadow: "0 4px 10px 0 rgba(0, 0, 0, 0.2)",
+      border: "1px solid rgba(255, 255, 255,0.1)",
+      backdropFilter: "blur(12px)"
     }
   },
   [`&.${stepConnectorClasses.completed}`]: {
     [`& .${stepConnectorClasses.line}`]: {
-      backgroundImage: `linear-gradient( 95deg,${theme.palette.success.main} 0%,${theme.palette.success.light} 50%,${theme.palette.success.main} 100%)`
+      backgroundImage: `linear-gradient(95deg,rgba(76, 175, 80, 0.55) 0%,rgba(46, 125, 50, 0.55) 50%,rgba(27, 94, 32, 0.55) 100%)`,
+      boxShadow: "0 4px 10px 0 rgba(0, 0, 0, 0.2)",
+      border: "1px solid rgba(255, 255, 255,0.1)",
+      backdropFilter: "blur(12px)"
     }
   },
   [`& .${stepConnectorClasses.line}`]: {
-    height: 3,
+    height: 5,
     border: 0,
-    backgroundImage: `linear-gradient( 95deg,${theme.palette.grey[500]} 0%,${theme.palette.grey[300]} 50%,${theme.palette.grey[500]} 100%)`,
+    backgroundImage: `linear-gradient(95deg,rgba(158, 158, 158, 0.55) 0%,rgba(224, 224, 224, 0.55) 50%,rgba(158, 158, 158, 0.55) 100%)`,
     borderRadius: 1
   }
 }));
--- src/components/steps/customStepIcon.style.ts
+++ src/components/steps/customStepIcon.style.ts
@@ -1,29 +1,33 @@
 import { styled } from "@mui/material/styles";
 
-export const CustomStepIconStyled = styled('div')<{
+export const CustomStepIconStyled = styled("div")<{
   ownerState: { completed?: boolean; active?: boolean };
 }>(({ theme, ownerState }) => ({
   zIndex: 1,
   color: theme.palette.common.white,
   width: theme.spacing(6),
   height: theme.spacing(6),
-  display: 'flex',
-  borderRadius: '50%',
-  justifyContent: 'center',
-  alignItems: 'center',
-  ...(!ownerState.active && !ownerState.completed && {
-    backgroundImage:
-      `linear-gradient( 136deg, ${theme.palette.grey[300]} 0%, ${theme.palette.grey[500]} 50%, ${theme.palette.grey[700]} 100%)`,
-    boxShadow: '0 4px 10px 0 rgba(0,0,0,.25)',
-  }),
+  display: "flex",
+  borderRadius: "50%",
+  justifyContent: "center",
+  alignItems: "center",
+  ...(!ownerState.active &&
+    !ownerState.completed && {
+      backgroundColor: "rgba(158, 158, 158, 0.4)",
+      boxShadow: "0 4px 10px 0 rgba(0, 0, 0, 0.1)",
+      border: "1px solid rgba(255, 255, 255,0.1)",
+      backdropFilter: "blur(12px)"
+    }),
   ...(ownerState.active && {
-    backgroundImage:
-      `linear-gradient( 136deg, ${theme.palette.primary.light} 0%, ${theme.palette.primary.main} 50%, ${theme.palette.primary.dark} 100%)`,
-    boxShadow: '0 4px 10px 0 rgba(0,0,0,.25)',
+    backgroundColor: "rgba(25, 118, 210, 0.4)",
+    boxShadow: "0 4px 10px 0 rgba(0, 0, 0, 0.1)",
+    border: "1px solid rgba(255, 255, 255,0.1)",
+    backdropFilter: "blur(12px)"
   }),
   ...(ownerState.completed && {
-    backgroundImage:
-      `linear-gradient( 136deg, ${theme.palette.success.light} 0%, ${theme.palette.success.main} 50%, ${theme.palette.success.dark} 100%)`,
-      boxShadow: '0 4px 10px 0 rgba(0,0,0,.25)',
-  }),
+    backgroundColor: "rgba(46, 125, 50, 0.4)",
+    boxShadow: "0 4px 10px 0 rgba(0, 0, 0, 0.1)",
+    border: "1px solid rgba(255, 255, 255,0.1)",
+    backdropFilter: "blur(12px)"
+  })
 }));
