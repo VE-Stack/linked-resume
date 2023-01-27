dd79bac933edb4143eeffe4b7d2b9d9399f46984
elnurastanov <elnurastanov@gmail.com>
2023-01-27 19:23:49 +0400

    Style overrided paper component

---
+++ src/theme/paper.ts
@@ -0,0 +1,17 @@
+import { Components, colors } from "@mui/material";
+
+export const paper:Components = {
+  MuiPaper: {
+    defaultProps: {
+      elevation: 16
+    },
+    styleOverrides: {
+      root: {
+        backgroundColor: "rgba(255,255,255, 0.4)",
+        backdropFilter: "blur(12px)",
+        border: "1px solid rgba(255,255,255,0.1)",
+        zIndex: 2
+      }
+    }
+  }
+}
