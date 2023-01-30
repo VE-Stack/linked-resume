51a26b8090e84cdad421d6812db314407eea17dd
MDReal32 <veysaliyev00@gmail.com>
2023-01-30 11:59:24 +0400

    env port setup

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const { VITE_SERVER_PORT = 3500, VITE_CLIENT_PORT = 3000 } = process.env;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({ origin: `http://localhost:${VITE_CLIENT_PORT}`, credentials: true });
+
+  await app.listen(VITE_SERVER_PORT);
 }
 bootstrap();
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
+const { VITE_CLIENT_PORT = 3000 } = process.env;
+
 export default defineConfig({
   plugins: [react()],
   build: { outDir: "build" },
+  server: {
+    port: +VITE_CLIENT_PORT
+  },
   test: {
     globals: true,
     environment: "jsdom",
